2c730665b0c3142ae61fb2e2caa927e92d9c3292
Stenver Jerkku <stenver1010@gmail.com>
2016-04-01 00:26:30 +0300

    Add comment box container test

--- lecture_3/mocha_testing_example/test/CommentBoxContainer-test.js
+++ lecture_3/mocha_testing_example/test/CommentBoxContainer-test.js
@@ -1,16 +1,9 @@
 'use strict';
 
-jest.unmock('../CommentBoxContainer');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import CommentBoxContainer from '../CommentBoxContainer';
-import CommentBox from '../CommentBox';
-
-// Pure functions need to be mocked to return some DOM
-// We give it a classname to test that it is rendered
-CommentBox.mockImplementation((props) => <div className="commentBox"></div>)
+import CommentBoxContainer from '../components/CommentBoxContainer';
 
 describe('CommentBoxContainer', () => {
   var buildCommentBoxContainer = (() => {
@@ -23,28 +16,28 @@ describe('CommentBoxContainer', () => {
     let commentBoxContainer = buildCommentBoxContainer();
 
     let comments = commentBoxContainer.state.comments;
-    expect(comments.length).toEqual(1);
-    expect(comments[0].text).toEqual("This is niss thing");
-    expect(comments[0].author).toEqual("Sukram");
+    expect(comments).to.have.lengthOf(1);
+    expect(comments[0]).to.have.property('author', 'Sukram');
+    expect(comments[0]).to.have.property('text', 'This is niss thing');
   });
 
   it('renders commentBox component', () => {
     let commentBoxContainer = buildCommentBoxContainer();
     let commentBox = TestUtils.findRenderedDOMComponentWithClass(commentBoxContainer, "commentBox")
 
-    expect(commentBox).toBeDefined();
+    expect(commentBox).to.exist;
   });
 
-  describe('when new comment is submitted', () => {
+  context('when new comment is submitted', () => {
     it('adds new comment to state', () => {
       let commentBoxContainer = buildCommentBoxContainer();
       let comment = {text: "Some text", author: "Sukram"}
       commentBoxContainer.handleCommentSubmit(comment);
 
       let comments = commentBoxContainer.state.comments;
-      expect(comments.length).toEqual(2);
-      expect(comments[1].text).toEqual("Some text");
-      expect(comments[1].author).toEqual("Sukram");
+      expect(comments).to.have.lengthOf(2)
+      expect(comments[1]).to.have.property('author', 'Sukram');
+      expect(comments[1]).to.have.property('text', 'Some text');
     });
   });
 });
